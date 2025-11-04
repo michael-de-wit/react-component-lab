56e9ca26f6e9ea95d4408284dfb0e75b68510fc5
michael-de-wit <michael.b.dewit@gmail.com>
2025-11-04 12:20:34 -0800

    initial pass WeatherIcon refactor

--- react-component-lab/src/components/WeatherForecast/WeatherForecast.jsx
+++ react-component-lab/src/components/WeatherForecast/WeatherForecast.jsx
@@ -2,6 +2,7 @@
 
 import './WeatherForecast.css';
 import WeatherData from './WeatherData/WeatherData.jsx';
+import WeatherIcon from './WeatherIcon/WeatherIcon.jsx';
 
 const WeatherForecast = (props) => {
     //   const {day, img, imgAlt, conditions, time} = props;
@@ -12,6 +13,7 @@ const WeatherForecast = (props) => {
             {dayArray.map((dayOfWeek, index) => (
                 <>
                     <h2>{dayOfWeek.day}</h2>
+                    <WeatherIcon key={index} img={dayOfWeek.img} imgAlt={dayOfWeek.imgAlt} />
                     <WeatherData key={index} conditions={dayOfWeek.conditions} time={dayOfWeek.time} />
                 </>
             ))}
